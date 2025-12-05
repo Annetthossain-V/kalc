9fffb11d3bea3b6a5dd5c6a10157030c1102aa79
Annett Hossain <annetthossain@outlook.com>
2025-12-05 20:27:10 +0600

    add file handler

--- src/mode.c
+++ src/mode.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stddef.h>
 #include <mode.h>
 #include <stdio.h>
@@ -5,9 +6,51 @@
 #include <string.h>
 #include <parser/lexer.h>
 
+static int buff_handler(const char* buff)
+{
+  size_t lexer_len = 0;
+  lexer_t** lexer = LexStr(buff, &lexer_len);
+  if (lexer == NULL) return 1;
+
+  LexFree(lexer, lexer_len);
+  return 0;
+}
+
 int FileMode(Cmdline_t* cmd)
 {
+  char* buff = (char*) malloc(1024);
+  for (uint16_t i = 0; i < cmd->FilesLen; ++i)
+  {
+    FILE* file = fopen(cmd->Files[i], "r");
+    if (file == NULL)
+    {
+      fprintf(stderr, "[ERR] Unable to open file `%s`\n", cmd->Files[i]);
+      free(buff);
+      return 0;
+    }
 
+    while (fgets(buff, 1024, file))
+    {
+      buff[strlen(buff) - 1] = '\0';
+      if (strcmp(buff, "exit") == 0)
+      {
+        fclose(file);
+        goto fini;
+      }
+
+      if (buff_handler(buff) != 0)
+      {
+        free(buff);
+        fclose(file);
+        return 0;
+      }
+    }
+
+    fclose(file);
+  }
+
+fini:
+  free(buff);
   return 1;
 }
 
@@ -23,15 +66,11 @@ int StdinMode(Cmdline_t* cmd)
     buff[strlen(buff) - 1] = '\0';
     if (strcmp(buff, "exit") == 0) break;
 
-    size_t lexer_len = 0;
-    lexer_t** lexer = LexStr(buff, &lexer_len);
-    if (lexer == NULL)
+    if (buff_handler(buff) != 0)
     {
       free(buff);
       return 0;
     }
-
-    LexFree(lexer, lexer_len);
   }
 
   free(buff);
